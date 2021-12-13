adbce856237b2f3baf624c6024ea55c8fffe35ae
조우준 <jowoojun77@gmail.com>
2021-12-13 16:04:09 +0900

    Feat: redux, redux-saga, styled-components 기본 설정

---
+++ pages/_document.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Document, {
+  Html, Head, Main, NextScript,
+  DocumentInitialProps, DocumentContext, 
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+// 스타일드 컴포넌트를 SSR하기 위해서는 Document를 직접 조작하는 방법 밖에 없다.
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    
+    try {
+      ctx.renderPage = () => originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Spartan:wght@400;500;700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
---
+++ sagas/index.js
@@ -0,0 +1,13 @@
+import { all, fork } from 'redux-saga/effects';
+import axios from 'axios';
+
+// import { backendURL } from '../config/config';
+
+// axios.defaults.baseURL = `${backendURL}/api`;
+// // 쿠키도 같이 전달하고 싶으면 true로 해야함. 쿠키 사용시 true 안하면 401에러가 뜸.
+// axios.defaults.withCredentials = true;
+
+export default function* rootSaga() {
+  yield all([
+  ]);
+}
